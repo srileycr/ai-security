eef8e333822207c9366ea70e5531c2a08a63cc2f
srileycr <jirenrulez@proton.me>
2026-06-25 04:57:20 +0000

    day1: strings practice

--- day1.py
+++ day1.py
@@ -1,12 +1,12 @@
 name = "Steven Riley"
 tool = "AISBREAKER"
 
-message = f({name} {tool} "This is a tool called: " 
+message = (f"My name is {name} and this is a {tool} called: ")
 
 print ("""Today Im going 
 to present something especial""")
 print ('Today\'s the day')
-print ('My name is ' + name + ' and today I will show yo tool called ' + tool.lower)
+print ('My name is ' + name + ' and today I will show yo tool called ' + tool.lower())
 print (len(name))
-print (name.find[Steven])
+print (name.count('Steven'))
 print (message)
